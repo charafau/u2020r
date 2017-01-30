ef7446b7118238240f0c946ca6e808ec10a0f554
charafau <gorudonu@gmail.com>
2017-01-31 02:52:04 +0900

    Added showing items after loading

--- src/componenets/homeScreen.js
+++ src/componenets/homeScreen.js
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 
 import * as actions from './../actions/repoActions';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 
 class HomeScreen extends React.Component {
@@ -17,23 +17,47 @@ class HomeScreen extends React.Component {
     };
 
     componentWillMount() {
+        console.log('mointing home screen');
         this.props.dispatch(actions.fetchRepos())
     }
 
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows(this.props.repos)
+        };
+
+    }
+
+    componentWillReceiveProps(newProps) {
+        let repos = newProps.repos;
+
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(repos)
+        });
+
+
+    }
 
     render() {
         console.log(this.props);
         return (
             <View>
                 <Text>Hello, Chat App!</Text>
-
+                <ListView
+                    dataSource={this.state.dataSource}
+                    removeClippedSubviews={false}
+                    enableEmptySections={true}
+                    renderRow={(rowData) => <Text>{rowData.name}</Text>}
+                />
             </View>
         );
     }
 }
 
 function mapStateToProps(state) {
-    return { repos: state.repos.repos };
+    return {repos: state.repos.repos};
 }
 
 export default connect(mapStateToProps)(HomeScreen);
